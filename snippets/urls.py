f03e27e3d81aefe9a18f8116ca86715e88fb91d0
MughalUsama <mughal.usama1@yahoo.com>
2021-10-01 15:02:04 +0500

    Completed Tutorial 6: ViewSets & Routers

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,23 +1,51 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
 # API endpoints
-urlpatterns = format_suffix_patterns([
-    path('', views.api_root),
-    path('snippets/',
-         views.SnippetList.as_view(),
-         name='snippet-list'),
-    path('snippets/<int:pk>/',
-         views.SnippetDetail.as_view(),
-         name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/',
-         views.SnippetHighlight.as_view(),
-         name='snippet-highlight'),
-    path('users/',
-         views.UserList.as_view(),
-         name='user-list'),
-    path('users/<int:pk>/',
-         views.UserDetail.as_view(),
-         name='user-detail')
-])
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+
+
+
+
+
+
+
+
+# snippet_list = views.SnippetViewSet.as_view({
+#     'get': 'list',
+#     'post': 'create'
+# })
+# snippet_detail = views.SnippetViewSet.as_view({
+#     'get': 'retrieve',
+#     'put': 'update',
+#     'patch': 'partial_update',
+#     'delete': 'destroy'
+# })
+# snippet_highlight = views.SnippetViewSet.as_view({
+#     'get': 'highlight'
+# }, renderer_classes=[renderers.StaticHTMLRenderer])
+# user_list = views.UserViewSet.as_view({
+#     'get': 'list'
+# })
+# user_detail = views.UserViewSet.as_view({
+#     'get': 'retrieve'
+# })
+# urlpatterns = format_suffix_patterns([
+#     path('', views.api_root),
+#     path('snippets/', snippet_list, name='snippet-list'),
+#     path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
+#     path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
+#     path('users/', user_list, name='user-list'),
+#     path('users/<int:pk>/', user_detail, name='user-detail')
+# ])
